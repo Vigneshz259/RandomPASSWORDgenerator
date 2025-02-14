b709f56e8572d51f75e63dbd7dba58ddb30cf059
Vigneshz259 <vignesh.anbalagan30@gmail.com>
2025-02-14 04:23:20 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function generate_password() {
   let password = "";
   let possible =
     "ABCDFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*";
-  for (i = 0; i < 8; i++) {
+  for (i = 0; i < 15; i++) {
     password += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   passwordInput.value = password;
