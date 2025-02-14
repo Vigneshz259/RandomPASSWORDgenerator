d4913e49af655684419c42813cee4b6f692ad509
Vigneshz259 <vignesh.anbalagan30@gmail.com>
2025-02-14 04:15:10 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let passwordButton = document.getElementById("btn_generate_password");
+
+let passwordInput = document.getElementById("input_password");
+
+function generate_password() {
+  let password = "";
+  let possible =
+    "ABCDFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*";
+  for (i = 0; i < 8; i++) {
+    password += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  passwordInput.value = password;
+}
